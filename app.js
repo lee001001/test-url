cdfc2af194005105c42d55a256c9e5d2aad48ee8
King LEE <a0988698776@gmail.com>
2020-10-16 21:25:30 +0800

    fetch: project init

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const exphbs = require('express-handlebars')
+
+const port = 3000
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(port, () => {
+  console.log(`Express is running on http://localhost:${port}`)
+})
